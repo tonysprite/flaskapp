214a44dc34b496120db90122449cf8eae3e1cc22
tangli <tangli.bj@acewill.cn>
2020-06-12 11:03:01 +0800

    payload post method

--- src/app.py
+++ src/app.py
@@ -14,27 +14,16 @@ app = Flask(__name__)
 @app.route("/")
 def show_details() :
     global startTime
-    return "<html>" + \
-           "<head><title>Docker + Flask Demo</title></head>" + \
-           "<body>" + \
-           "<table>" + \
-           "<tr><td> Start Time </td> <td>" +  startTime + "</td> </tr>" \
-           "<tr><td> Hostname </td> <td>" + utils.gethostname() + "</td> </tr>" \
-           "<tr><td> Local Address </td> <td>" + utils.getlocaladdress() + "</td> </tr>" \
-           "<tr><td> Remote Address </td> <td>" + request.remote_addr + "</td> </tr>" \
-           "<tr><td> Server Hit </td> <td>" + str(hit.getServerHitCount()) + "</td> </tr>" \
-           "</table>" + \
-           "</body>" + \
-           "</html>"
+    return "hello"
 
 @app.route("/json")
 def send_json() :
     global startTime
     return jsonify( {'StartTime' : startTime,
-                     'Hostname': utils.gethostname(),
-                     'LocalAddress': utils.getlocaladdress(),
-                     'RemoteAddress':  request.remote_addr,
                      'Server Hit': str(hit.getServerHitCount())} )
 
+@app.route("/payload")
+def payload() :
+	return str(request.get_data(True,True,False))
 if __name__ == "__main__":
     app.run(debug = True, host = '0.0.0.0')
